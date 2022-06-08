c45e5208717435693bf0bf8145c6e3b8139936bc
Giuseppe De Marco <giuseppe.demarco@teamdigitale.governo.it>
2022-06-08 14:55:46 +0200

    fix: build preview

--- preview_build/build.sh
+++ preview_build/build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+pip install -r requirements.txt
+
 rm -rf ./build && \
 cp ./preview_configuration.py ./conf.py && \
-sphinx-build .. build -c .
+sphinx-build -b html -d build/it/doctrees ../docs/it/ build/it
