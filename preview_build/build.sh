d40be2c56a4d86dfd2bafd4ac1d5a980e9ffa31d
Giuseppe De Marco <giuseppe.demarco@teamdigitale.governo.it>
2022-07-23 11:55:00 +0200

    feat: vercel previw in both en and it

--- preview_build/build.sh
+++ preview_build/build.sh
@@ -4,4 +4,7 @@ pip install -r requirements.txt
 
 rm -rf ./build && \
 cp ./preview_configuration.py ./conf.py && \
-sphinx-build -b html -d build/doctrees ../docs/it/ build
+
+# sphinx-build -b html -d build/doctrees ../docs/it/ build
+sphinx-build -b html -d build/en/doctrees ../docs/en/ build/en
+sphinx-build -b html -d build/it/doctrees ../docs/it/ build/it
