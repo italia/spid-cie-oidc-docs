b79bef0ed68073fced42523655e90b8334897b24
Giuseppe De Marco <demarcog83@gmail.com>
2022-09-29 12:03:06 +0200

    chore: Sphinx v5.0.1 test

--- docs/en/conf.py
+++ docs/en/conf.py
@@ -166,7 +166,7 @@ html_static_path = ['../../static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ["../common"]
+# html_extra_path = ["../common"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
